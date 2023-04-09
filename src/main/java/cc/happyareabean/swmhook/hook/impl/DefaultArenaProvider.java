d62e82446fbd6496e196d054a30b39c9aa78cb61
HappyAreaBean <bean@happyareabean.cc>
2023-04-10 02:48:41 +0800

    feat(default-arena): add provider author and version

--- src/main/java/cc/happyareabean/swmhook/hook/impl/DefaultArenaProvider.java
+++ src/main/java/cc/happyareabean/swmhook/hook/impl/DefaultArenaProvider.java
@@ -1,5 +1,6 @@
 package cc.happyareabean.swmhook.hook.impl;
 
+import cc.happyareabean.swmhook.constants.Constants;
 import cc.happyareabean.swmhook.hook.ArenaProvider;
 import cc.happyareabean.swmhook.objects.SWMHWorld;
 
@@ -13,5 +14,15 @@ public class DefaultArenaProvider extends ArenaProvider {
 	@Override
 	public String getProviderName() {
 		return "Default";
+	}
+
+	@Override
+	public String getProviderAuthor() {
+		return "HappyAreaBean";
+	}
+
+	@Override
+	public String getProviderVersion() {
+		return Constants.VERSION;
 	}
 }
